82549d2e4385b4ce4993e6279e1698bd7d85cc89
Kareem <kareem0018@gmail.com>
2024-03-08 16:56:02 +0200

    Initializing the project

---
+++ config/credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dbUri: "mongodb+srv://<username>:<password>@<cluster>.4rc4s.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    dbCluster: "softwarecluster",
+    dbUsername: "admin",
+    dbPassword: "admin1234",
+    dbMain: "dataset"
+}
