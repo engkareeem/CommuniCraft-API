8c707af7a4fb49f16d9e9a84d92604a8d4e2f531
NasrAllah <shaer.nasrallah@gmail.com>
2024-03-19 23:53:26 +0200

    Added Auth and Login/Signup Functionality

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require("dotenv").config()
 
 const db = require('./config/dbconnection')
 const express = require('express')
+const routers = require('./routes.js')
+const cookieParser = require("cookie-parser")
 const app = express()
 
-const routers = require('./routes.js')
 
+
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", routers);
 
 app.listen(8000)
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -18,7 +20,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password'],
-        minLength: [6, 'Minimum password length is 6']
+        minlength: [6, 'Minimum password length is 6']
     },
     skills: [String],
     interests: [String],
@@ -30,6 +32,24 @@ const userSchema = new mongoose.Schema({
     }],
 });
 
+userSchema.pre('save', async function(next){
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({email});
+    if(user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth) {
+            return user;
+        }
+        throw Error("Incorrect password");
+    }
+    
+    throw Error("Email not found");
+    
+}
 
 
 module.exports = mongoose.model("User", userSchema,"Users");
